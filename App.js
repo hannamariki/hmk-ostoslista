ab961920fa809d60340a57cee3bd0fc337d6f447
Hanna-Mari Kielenniva <h.kielenniva@gmail.com>
2024-09-02 13:37:31 +0300

    Tehtävä valmis

--- App.js
+++ App.js
@@ -1,20 +1,90 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView, View, TextInput, Button, FlatList, Text, StyleSheet } from 'react-native';
 
 export default function App() {
+  const [item, setItem] = useState('');
+  const [shoppingList, setShoppingList] = useState([]);
+
+ 
+  const addItem = () => {
+    if (item.trim().length > 0) {
+      setShoppingList([...shoppingList, item.trim()]);
+      setItem('');
+    }
+  };
+
+
+  const clearList = () => {
+    setShoppingList([]);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder=" "
+          value={item}
+          onChangeText={setItem}
+        />
+      </View>
+      
+      <View style={styles.buttonContainer}>
+        <Button title="Add" onPress={addItem} />
+        <Button title="Clear" onPress={clearList} />
+      </View>
+
+      <Text style={styles.shopTitle}>Shopping List</Text>
+
+      <FlatList
+        data={shoppingList}
+        renderItem={({ item }) => <Text style={styles.listItem}>{item}</Text>}
+        keyExtractor={(item, index) => index.toString()}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    padding: 16,
+    marginTop: 50, 
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  inputContainer: {
+    marginBottom: 16,
+    width: '100%',
+  },
+  input: {
+    height: 40,
+    borderColor: 'grey',
+    borderWidth: 1,
+    paddingHorizontal: 8,
+    textAlign: "center",
+    width: '100%',
+  },
+  buttonContainer: {
+    flexDirection: 'row',       
+    justifyContent: 'center',   
+    marginVertical: 16,         
+  },
+  listItem: {
+    fontSize: 18,
+    textAlign: 'left',
+  
+  },
+  shopTitle: { 
+    textAlign: 'center',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  shopItem: {
+    textAlign: 'center',
+    padding: 10,
+    fontSize: 16,
+  },
+  list: {
+    alignItems: 'center', 
   },
 });
